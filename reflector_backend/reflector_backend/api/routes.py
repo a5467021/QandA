db7fc08ccff1228c7d731738ed8a6c6907a042d6
a5467021 <a5467021@163.com>
2018-02-28 11:13:25 +0800

    fixed display error

--- reflector_backend/reflector_backend/api/routes.py
+++ reflector_backend/reflector_backend/api/routes.py
@@ -1,1 +1,42 @@
-'''Customized RESTful api routes.'''import jsonfrom flask import request, make_responsefrom reflector_backend.views import *from reflector_backend.api.process import *def pack(data = ''): # solves the access-control problem and declares the type of data    res = make_response(data);    res.mimetype = 'application/json';    res.headers['Access-Control-Allow-Origin'] = '*';    res.headers['Access-Control-Allow-Methods'] = 'GET, POST';    res.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type';     return res;@app.route('/api/question', methods = ['GET'])def api_question_test():    return '''           <form action="/api/question" method="post">               <input type="checkbox" name="category" value="音乐">音乐</input>               <input type="checkbox" name="category" value="电影">电影</input>               <input type="checkbox" name="category" value="科技">科技</input>               <input type="checkbox" name="category" value="阅读">阅读</input>               <input type="checkbox" name="category" value="游戏">游戏</input>               <input type="checkbox" name="category" value="时事">时事</input>               <input type="checkbox" name="category" value="其他">其他</input>               <input type="hidden" name="type" value="test"></input>               <input type="submit" value="提交"></input>           </form>           ''';@app.route('/api/question', methods = ['POST'])def api_question():    if request.form.get('type') == 'test':        category = request.form.getlist('category');    else:        form = json.loads(request.data);        category = form['category'];    return pack(json.dumps(GetQuestion(category)));
+'''
+Customized RESTful api routes.
+'''
+
+import json
+from flask import request, make_response
+from reflector_backend.views import *
+from reflector_backend.api.process import *
+
+
+def pack(data = ''): # solves the access-control problem and declares the type of data
+    res = make_response(data);
+    res.mimetype = 'application/json';
+    res.headers['Access-Control-Allow-Origin'] = '*';
+    res.headers['Access-Control-Allow-Methods'] = 'GET, POST';
+    res.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'; 
+    return res;
+
+@app.route('/api/question', methods = ['GET'])
+def api_question_test():
+    return '''
+           <form action="/api/question" method="post">
+               <input type="checkbox" name="category" value="音乐">音乐</input>
+               <input type="checkbox" name="category" value="电影">电影</input>
+               <input type="checkbox" name="category" value="科技">科技</input>
+               <input type="checkbox" name="category" value="阅读">阅读</input>
+               <input type="checkbox" name="category" value="游戏">游戏</input>
+               <input type="checkbox" name="category" value="时事">时事</input>
+               <input type="checkbox" name="category" value="其他">其他</input>
+               <input type="hidden" name="type" value="test"></input>
+               <input type="submit" value="提交"></input>
+           </form>
+           ''';
+
+@app.route('/api/question', methods = ['POST'])
+def api_question():
+    if request.form.get('type') == 'test':
+        category = request.form.getlist('category');
+    else:
+        form = json.loads(request.data);
+        category = form['category'];
+    return pack(json.dumps(GetQuestion(category)));
